452ace08b0ba15dce5ee3c454392bb2554550565
Himada4 <45938024+Himada4@users.noreply.github.com>
2024-01-07 23:48:01 +0100

    lyrics added

--- Foundation.cs
+++ Foundation.cs
@@ -7,6 +7,7 @@ using StorybrewCommon.Storyboarding.Util;
 using StorybrewCommon.Subtitles;
 using StorybrewCommon.Util;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -64,9 +65,217 @@ namespace StorybrewScripts
                  
             }
 
-
+            introShapes(mono);
 		    
+            var blur = bgLayer.CreateSprite("sb/blur.jpg");
+            blur.Scale(11454, 480.0f / bit.Height);
+            blur.Fade(11454, 0.7);
+            blur.Fade(55340, 0);
+            WiggleScreen2(11454, 55340, 50, new Vector2(320, 240), 0, 2, blur);
             
+        }
+
+        void introShapes(StoryboardLayer layer){
+
+            var triangle = new List<OsbSprite>(){layer.CreateSprite("sb/t.png",OsbOrigin.Centre, new Vector2(320, 242)), layer.CreateSprite("sb/t.png",OsbOrigin.Centre, new Vector2(320, 242))};
+            var circle = new List<OsbSprite>(){layer.CreateSprite("sb/c.png"), layer.CreateSprite("sb/c.png")};
+            var sqare = new List<OsbSprite>(){layer.CreateSprite("sb/p.png"), layer.CreateSprite("sb/p.png")};
+            var cross = new List<OsbSprite>(){layer.CreateSprite("sb/p.png"), layer.CreateSprite("sb/p.png")};
+
+            List<int> pluck = new List<int>(){
+                483, 1168, 1340, 1511, 1682, 
+                3225, 3911, 4083, 4254, 4425, 4768, 4940, 5111, 5454, 5625, 5797,
+                6654, 6825, 6997, 7168, 8025, 8197, 8368, 8540, 11454
+            };
+
+            int c = 0;
+            foreach(var t in triangle){
+                t.Scale(OsbEasing.OutExpo, 483, 1168, 0, c == 0 ? 0.15 : 0.135);
+                if(c != 0) t.Color(483, Color4.Black);
+                c++;
+            }
+            c = 0;
+            WiggleScreen(483, 11454, 150, new Vector2(320, 240), 0, 0.5f, triangle[0], triangle[1]);
+            
+            
+            foreach(var t in circle){
+                t.Scale(1168, c == 0 ? 0.05 : 0.045);
+                if(c != 0) t.Color(1168, Color4.Black);
+                c++;
+            }
+            c = 0;
+            WiggleScreen(1168, 11454, 150, new Vector2(320, 240), 0, 0.5f, circle[0], circle[1]);
+                    
+                   
+            foreach(var t in sqare){
+                t.Scale(1340, c == 0 ? 15 : 14);
+                if(c != 0) 
+                    t.Color(1340, Color4.Black);
+                //t.Rotate(1340, Math.PI/4);
+                c++;
+            }
+            c = 0;
+            WiggleScreen(1340, 11454, 150, new Vector2(320, 240), Math.PI/4, 0.5f, sqare[0], sqare[1]);
+            
+            
+            foreach(var t in cross){
+                t.ScaleVec(1511, 1, 18);
+                //t.Rotate(1511, c == 0 ? -Math.PI/4 : Math.PI/4);
+                t.ScaleVec(OsbEasing.InExpo, 8540, 11454, 1, 18, 1080, 480);
+                
+                c++;
+            }
+            WiggleScreen(1511, 11454, 150, new Vector2(320, 240), -Math.PI/4, 0.5f, cross[0]);
+            WiggleScreen(1511, 11454, 150, new Vector2(320, 240), Math.PI/4, 0.5f, cross[1]);
+            
+
+            int count = 0;
+            for(var i = 0; i < pluck.Count - 1; i ++){
+                switch(count){
+                    case 0:
+                        foreach(var t in triangle){
+                            t.Fade(pluck[i], 1);
+                            t.Fade(pluck[i + 1], 0);
+                        }
+                    break;
+                    case 1:
+                        foreach(var t in circle){
+                            t.Fade(pluck[i], 1);
+                            t.Fade(pluck[i + 1], 0);
+                        }
+                    break;
+                    case 2:
+                        foreach(var t in sqare){
+                            t.Fade(pluck[i], 1);
+                            t.Fade(pluck[i + 1], 0);
+                        }
+                    break;
+                    case 3:
+                        foreach(var t in cross){
+                            if(i == pluck.Count-2){
+                                t.Fade(pluck[i], 1);
+                                t.Fade(OsbEasing.OutExpo, pluck[i + 1],pluck[i + 1] + 600, 1, 0);
+                            }else{
+                                t.Fade(pluck[i], 1);
+                                t.Fade(pluck[i + 1], 0);
+                            }
+                            
+                        }
+                    break;
+                }
+                if(count == 3) count = 0;
+                else count++;
+            }
+        }
+
+        public void WiggleScreen(double startTime, double endTime, int rate, Vector2 InitPos, double InitRot, float wiggleAmount, params OsbSprite[] sprites){
+
+            //Rate average around 20 to 100 (bigger number, more shakes)
+
+            var loopTime = (endTime-startTime)/rate;
+
+            var previousCord = new Vector2(InitPos.X,InitPos.Y);
+
+            for(int i = 0; i < rate - 1; i++){
+
+                var xCord = Random(InitPos.X-wiggleAmount,InitPos.X+wiggleAmount);
+
+                var yCord = Random(InitPos.Y-wiggleAmount,InitPos.Y+wiggleAmount);
+
+                var tempCord = new Vector2(xCord,yCord);
+                
+                foreach(var sprite in sprites){
+                    sprite.Move(OsbEasing.InOutSine,startTime+(loopTime*i),startTime+(loopTime*(i+1)),previousCord,tempCord);
+                }
+            
+                //Log($"{startTime+(loopTime*i)} until {startTime+(loopTime*(i+1))}");
+
+                previousCord = tempCord;
+                
+            }
+
+            double previousRotation = InitRot;
+
+            for(int i = 0; i < (rate - 1)/2; i++){
+
+                double[] rotate = new double[]{
+                    0.01 // , 0.02, 0.03 // Add these for more rotations
+                };
+
+                var rotInd = Random(0,rotate.Length);
+
+                var tempRot = previousRotation - rotate[rotInd];
+
+                foreach(var sprite in sprites){
+                sprite.Rotate(OsbEasing.InOutSine,startTime+(2*loopTime*i),startTime+(2*loopTime*(i+1)),previousRotation,tempRot);
+                }
+
+                previousRotation = tempRot;
+
+            }
+
+            foreach(var sprite in sprites){
+
+                sprite.Rotate(OsbEasing.InOutSine, startTime + loopTime * (rate - 1), startTime +  loopTime * rate, previousRotation, 0);
+                sprite.Move(OsbEasing.InOutSine,  startTime + loopTime * (rate - 1),  startTime + loopTime * rate , previousCord, InitPos);
+
+            }
+
+        }
+
+        public void WiggleScreen2(double startTime, double endTime, int rate, Vector2 InitPos, double InitRot, float wiggleAmount, params OsbSprite[] sprites){
+
+            //Rate average around 20 to 100 (bigger number, more shakes)
+
+            var loopTime = (endTime-startTime)/rate;
+
+            var previousCord = new Vector2(InitPos.X,InitPos.Y);
+
+            for(int i = 0; i < rate - 1; i++){
+
+                var xCord = Random(InitPos.X-wiggleAmount,InitPos.X+wiggleAmount);
+
+                var yCord = Random(InitPos.Y-wiggleAmount,InitPos.Y+wiggleAmount);
+
+                var tempCord = new Vector2(xCord,yCord);
+                
+                foreach(var sprite in sprites){
+                    sprite.Move(OsbEasing.InOutSine,startTime+(loopTime*i),startTime+(loopTime*(i+1)),previousCord,tempCord);
+                }
+            
+                //Log($"{startTime+(loopTime*i)} until {startTime+(loopTime*(i+1))}");
+
+                previousCord = tempCord;
+                
+            }
+
+            double previousRotation = InitRot;
+
+            for(int i = 0; i < (rate - 1)/2; i++){
+
+                double[] rotate = new double[]{
+                    0.01 // , 0.02, 0.03 // Add these for more rotations
+                };
+
+                var rotInd = Random(0,rotate.Length);
+
+                var tempRot = rotate[rotInd];
+
+                foreach(var sprite in sprites){
+                sprite.Rotate(OsbEasing.InOutSine,startTime+(2*loopTime*i),startTime+(2*loopTime*(i+1)),previousRotation,tempRot);
+                }
+
+                previousRotation = tempRot;
+
+            }
+
+            foreach(var sprite in sprites){
+
+                sprite.Rotate(OsbEasing.InOutSine, startTime + loopTime * (rate - 1), startTime +  loopTime * rate, previousRotation, 0);
+                sprite.Move(OsbEasing.InOutSine,  startTime + loopTime * (rate - 1),  startTime + loopTime * rate , previousCord, InitPos);
+
+            }
+
         }
     }
 }
---
+++ Karaoke.cs
@@ -0,0 +1,181 @@
+using OpenTK;
+using OpenTK.Graphics;
+using StorybrewCommon.Scripting;
+using StorybrewCommon.Storyboarding;
+using StorybrewCommon.Subtitles;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace StorybrewScripts
+{
+    public class Karaoke : StoryboardObjectGenerator
+    {
+        [Description("Path to a .sbv, .srt, .ass or .ssa file in your project's folder.\nThese can be made with a tool like aegisub.")]
+        [Configurable] public string SubtitlesPath = "lyrics.srt";
+        [Configurable] public float SubtitleY = 400;
+
+        [Group("Font")]
+        [Description("The name of a system font, or the path to a font relative to your project's folder.\nIt is preferable to add fonts to the project folder and use their file name rather than installing fonts.")]
+        [Configurable] public string FontName = "Verdana";
+        [Description("A path inside your mapset's folder where lyrics images will be generated.")]
+        [Configurable] public string SpritesPath = "sb/f";
+        [Description("The Size of the font.\nIncreasing the font size creates larger images.")]
+        [Configurable] public int FontSize = 26;
+        [Description("The Scale of the font.\nIncreasing the font scale does not creates larger images, but the result may be blurrier.")]
+        [Configurable] public float FontScale = 0.5f;
+        [Configurable] public Color4 FontColor = Color4.White;
+        [Configurable] public FontStyle FontStyle = FontStyle.Regular;
+        
+        [Group("Outline")]
+        [Configurable] public int OutlineThickness = 3;
+        [Configurable] public Color4 OutlineColor = new Color4(50, 50, 50, 200);
+
+        [Group("Shadow")]
+        [Configurable] public int ShadowThickness = 0;
+        [Configurable] public Color4 ShadowColor = new Color4(0, 0, 0, 100);
+
+        [Group("Glow")]
+        [Configurable] public int GlowRadius = 0;
+        [Configurable] public Color4 GlowColor = new Color4(255, 255, 255, 100);
+        [Configurable] public bool GlowAdditive = true;
+
+        [Group("Misc")]
+        [Configurable] public bool TrimTransparency = true;
+        [Configurable] public bool EffectsOnly = false;
+        [Description("How much extra space is allocated around the text when generating it.\nShould be increased when characters look cut off.")]
+        [Configurable] public Vector2 Padding = Vector2.Zero;
+        [Configurable] public OsbOrigin Origin = OsbOrigin.Centre;
+
+        public override void Generate()
+        {
+            var font = LoadFont(SpritesPath, new FontDescription()
+            {
+                FontPath = FontName,
+                FontSize = FontSize,
+                Color = FontColor,
+                Padding = Padding,
+                FontStyle = FontStyle,
+                TrimTransparency = TrimTransparency,
+                EffectsOnly = EffectsOnly,
+            },
+            new FontGlow()
+            {
+                Radius = GlowAdditive ? 0 : GlowRadius,
+                Color = GlowColor,
+            },
+            new FontOutline()
+            {
+                Thickness = OutlineThickness,
+                Color = OutlineColor,
+            },
+            new FontShadow()
+            {
+                Thickness = ShadowThickness,
+                Color = ShadowColor,
+            });
+
+            var subtitles = LoadSubtitles(SubtitlesPath);
+
+            if (GlowRadius > 0 && GlowAdditive)
+            {
+                var glowFont = LoadFont(Path.Combine(SpritesPath, "glow"), new FontDescription()
+                {
+                    FontPath = FontName,
+                    FontSize = FontSize,
+                    Color = FontColor,
+                    Padding = Padding,
+                    FontStyle = FontStyle,
+                    TrimTransparency = TrimTransparency,
+                    EffectsOnly = true,
+                },
+                new FontGlow()
+                {
+                    Radius = GlowRadius,
+                    Color = GlowColor,
+                });
+                generateLyrics(glowFont, subtitles, "glow", true);
+            }
+            generateLyrics(font, subtitles, "", false);
+        }
+
+        public void generateLyrics(FontGenerator font, SubtitleSet subtitles, string layerName, bool additive)
+        {
+            var regex = new Regex(@"({\\k(\d+)})?([^{]+)");
+
+            var layer = GetLayer(layerName);
+            var subY = new List<float>(){360, 380, 400};
+            var count = 0;
+            foreach (var subtitleLine in subtitles.Lines)
+            {
+                var letterY = subY[count];
+                foreach (var line in subtitleLine.Text.Split('\n'))
+                {
+                    var matches = regex.Matches(line.ToUpper());
+
+                    var lineWidth = 0f;
+                    var lineHeight = 0f;
+                    foreach (Match match in matches)
+                    {
+                        var text = match.Groups[3].Value;
+                        foreach (var letter in text)
+                        {
+                            var texture = font.GetTexture(letter.ToString());
+                            lineWidth += texture.BaseWidth * FontScale;
+                            lineHeight = Math.Max(lineHeight, texture.BaseHeight * FontScale);
+                        }
+                    }
+
+                    var karaokeStartTime = subtitleLine.StartTime;
+                    var letterX = 640 - lineWidth;
+                    foreach (Match match in matches)
+                    {
+                        var durationString = match.Groups[2].Value;
+                        var duration = string.IsNullOrEmpty(durationString) ? subtitleLine.EndTime - subtitleLine.StartTime : int.Parse(durationString) * 10;
+                        var karaokeEndTime = karaokeStartTime + duration;
+
+                        var text = match.Groups[3].Value;
+                        var delay = 0;
+                        foreach (var letter in text)
+                        {
+                            var texture = font.GetTexture(letter.ToString());
+                            if (!texture.IsEmpty)
+                            {
+                                var position = new Vector2(letterX, letterY)
+                                    + texture.OffsetFor(Origin) * FontScale;
+
+                                var sprite = layer.CreateSprite(texture.Path, Origin, position);
+                                sprite.Scale(subtitleLine.StartTime, FontScale);
+                                sprite.Fade(subtitleLine.StartTime - 200 + delay, subtitleLine.StartTime + delay, 0, 1);
+                                sprite.Fade(subtitleLine.EndTime - 200, subtitleLine.EndTime, 1, 0);
+                                sprite.MoveY(OsbEasing.OutBack, subtitleLine.StartTime - 200 + delay, subtitleLine.EndTime, position.Y + 10, position.Y);
+                                if (additive)
+                                    sprite.Additive(subtitleLine.StartTime - 200, subtitleLine.EndTime);
+
+                                applyKaraoke(sprite, subtitleLine, karaokeStartTime, karaokeEndTime);
+                            }
+                            letterX += texture.BaseWidth * FontScale;
+                            delay += 50;
+                        }
+                        karaokeStartTime += duration;
+                    }
+                    letterY += lineHeight;
+                }
+                if(count == 2) count = 0;
+                else count++;
+
+            }
+        }
+
+        private void applyKaraoke(OsbSprite sprite, SubtitleLine subtitleLine, double startTime, double endTime)
+        {
+            var before = new Color4(.2f, .2f, .2f, 1f);
+            var after = new Color4(.6f, .6f, .6f, 1f);
+
+            sprite.Color(startTime - 100, startTime, before, Color.White);
+            sprite.Color(endTime - 100, endTime, Color.White, after);
+        }
+    }
+}
